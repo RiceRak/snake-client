4d475461e53e5d4d003958f6d2bcf311543bb834
Shadrak Lagadi <shadrl19@gmail.com>
2023-12-13 14:05:32 -0700

    practice ES6 shorthand for export/require

--- client.js
+++ client.js
@@ -1,5 +1,6 @@
+const net = require("net");
 // establishes a connection with the game server
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
     host: "localhost",
     port: 50541,
@@ -14,3 +15,8 @@ const connect = function () {
   });
   return conn;
 };
+
+// module.exports = connect;
+module.exports = { 
+  connect,
+  }
