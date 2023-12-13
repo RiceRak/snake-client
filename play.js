2079a3a6ca0f2f6287e495da8d05702a7f9e8b63
Shadrak Lagadi <shadrl19@gmail.com>
2023-12-13 16:54:14 -0700

    moved setupInput and handleUserInput functions to input.js

--- play.js
+++ play.js
@@ -1,25 +1,8 @@
 const { connect } = require("./client")
-// setup interface to handle user input from stdin
-
-const handleUserInput = function (buffer) {
-  const key = buffer.toString();
-  console.log(key);
-  
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setDefaultEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
+const { setupInput } = require("./input") 
 
 
 console.log("Connecting ...");
+connect()
+
 setupInput();
