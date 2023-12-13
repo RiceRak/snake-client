061a5b947a690b607b3cf4899119b6f6de041289
Shadrak Lagadi <shadrl19@gmail.com>
2023-12-13 16:44:19 -0700

    fixed stdin setup and added ctr+c to quit

--- play.js
+++ play.js
@@ -1,12 +1,25 @@
 const { connect } = require("./client")
 // setup interface to handle user input from stdin
+
+const handleUserInput = function (buffer) {
+  const key = buffer.toString();
+  console.log(key);
+  
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
 const setupInput = function () {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setDefaultEncoding("utf8");
   stdin.resume();
+  stdin.on("data", handleUserInput);
   return stdin;
 };
 
+
+
 console.log("Connecting ...");
-connect();
+setupInput();
