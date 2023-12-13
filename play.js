881583b1abe37344f177aa8d667720937814f659
Shadrak Lagadi <shadrl19@gmail.com>
2023-12-13 12:33:14 -0700

    connected to the server & updated function to handle incoming data and to console log it

---
+++ play.js
@@ -0,0 +1,21 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  // update function to handle incoming data and console.log it for the player
+  conn.on("data", (data) => {
+    console.log("Message from server:", data);
+  });
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
