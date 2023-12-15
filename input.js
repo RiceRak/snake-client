b62d265a42ced6183221cccaeff4aa4b4d39c279
Shadrak Lagadi <shadrl19@gmail.com>
2023-12-14 17:22:48 -0700

    required movement and emoji keys and updated variables

--- input.js
+++ input.js
@@ -1,37 +1,30 @@
+const { MOVE_UP, MOVE_LEFT, MOVE_DOWN, MOVE_RIGHT, SAY_CRY, SAY_HAPPY } = require("./constants");
 let connection;
 
 const handleUserInput = function (buffer) {
   const key = buffer.toString();
   
-  
+
   if (key === '\u0003') {
     process.exit();
   }
-  
-  const w = "Move: up";
-  const a = "Move: left";
-  const s = "Move: down";
-  const d = "Move: right";
-  const q = "Say: QQ"
-  const e = "Say: =D"
-
   if (key === 'w') {
-    connection.write(w);
+    connection.write(MOVE_UP);
   }
   if (key === 'a') {
-    connection.write(a);
+    connection.write(MOVE_LEFT);
   }
   if (key === 's') {
-    connection.write(s);
+    connection.write(MOVE_DOWN);
   }
   if (key === 'd') {
-    connection.write(d);
+    connection.write(MOVE_RIGHT);
   }
   if (key === 'q') {
-    connection.write(q);
+    connection.write(SAY_CRY);
   }
   if (key === 'e') {
-    connection.write(e);
+    connection.write(SAY_HAPPY);
   }
   
 };
