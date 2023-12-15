980cf09f26f9024bd8f2e24b0b5b9ca080a31e8a
Shadrak Lagadi <shadrl19@gmail.com>
2023-12-14 17:21:56 -0700

    added movement keys and also renamed them

--- constants.js
+++ constants.js
@@ -1,7 +1,19 @@
 const IP = "localhost";
-const PORT = 50541; 
+const PORT = 50541;
+const MOVE_UP = "Move: up";
+const MOVE_LEFT = "Move: left";
+const MOVE_DOWN = "Move: down";
+const MOVE_RIGHT = "Move: right";
+const SAY_CRY = "Say: QQ";
+const SAY_HAPPY = "Say: =D";
 
 module.exports = {
   IP,
   PORT,
+  MOVE_UP,
+  MOVE_LEFT,
+  MOVE_DOWN,
+  MOVE_RIGHT,
+  SAY_CRY,
+  SAY_HAPPY,
 }
