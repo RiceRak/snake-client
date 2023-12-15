cf093fe71a5af4d5d94d0de867e08ea0f813b6c9
Shadrak Lagadi <shadrl19@gmail.com>
2023-12-14 18:34:34 -0700

    linted

--- client.js
+++ client.js
@@ -1,5 +1,6 @@
 const { IP, PORT } = require("./constants");
 const net = require("net");
+
 // establishes a connection with the game server
 const connect = function() {
   const conn = net.createConnection({
@@ -9,14 +10,12 @@ const connect = function() {
   
   // interpret incoming data as text
   conn.setEncoding("utf8");
-  
   // confirm that client/user has connected to the game
   conn.on("connect", () => {
     console.log("Successfully connected to game server!");
     //Send a name to the server
     conn.write("Name: RAK");
   });
-  
   // update function to handle incoming data and console.log it for the player
   conn.on("data", (data) => {
     console.log("Message from server:", data);
@@ -25,7 +24,6 @@ const connect = function() {
 
 };
 
-// module.exports = connect;
-module.exports = { 
+module.exports = {
   connect,
-  }
+};
