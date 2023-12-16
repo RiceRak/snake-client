11beb0502d07d7a9dd0aad8a35fc6def5f13dac1
Shadrak Lagadi <shadrl19@gmail.com>
2023-12-15 19:14:08 -0700

    removed debugging and step notes

--- client.js
+++ client.js
@@ -1,29 +1,24 @@
 const { IP, PORT } = require("./constants");
 const net = require("net");
-console.log(process.argv);
-const arg = process.argv
+
+const arg = process.argv;
 let playerName = "RAK";
 if (arg.length > 2) {
   playerName = arg[2];
 }
-// establishes a connection with the game server
-const connect = function () {
+
+const connect = function() {
   const conn = net.createConnection({
     host: IP,
     port: PORT,
   });
 
-  // interpret incoming data as text
   conn.setEncoding("utf8");
-  // confirm that client/user has connected to the game
   conn.on("connect", () => {
     console.log("Successfully connected to game server!");
-    //Send a name to the server
     conn.write(`Name: ${playerName}`);
-
-
   });
-  // update function to handle incoming data and console.log it for the player
+
   conn.on("data", (data) => {
     console.log("Message from server:", data);
   });
